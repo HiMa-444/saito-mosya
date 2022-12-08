8543d7c87793ddf0507dbaf770430219a7428b30
EC2 Default User <ec2-user@ip-172-31-20-214.ap-northeast-1.compute.internal>
2022-12-08 14:09:50 +0000

    サイト模写　課題提出

---
+++ kadai1.js
@@ -0,0 +1,7 @@
+function tgl() {
+  let tglopa = document.getElementById("tgl-opa");
+  let screen = document.getElementById("tgl-sc");
+  tglopa.classList.toggle("view");
+  screen.classList.toggle("screen");
+ 
+}
