4b88726b0adc9ade0db78a14d2c5ea502e421bb8
EC2 Default User <ec2-user@ip-172-31-20-214.ap-northeast-1.compute.internal>
2022-12-16 18:06:23 +0000

    768pxでのレスポンシブ対応の追加

--- kadai1.js
+++ kadai1.js
@@ -3,5 +3,10 @@ function tgl() {
   let screen = document.getElementById("tgl-sc");
   tglopa.classList.toggle("view");
   screen.classList.toggle("screen");
- 
+}
+function tgl1() {
+  let tglopa = document.getElementById("tgl-opa1");
+  let screen = document.getElementById("tgl-sc1");
+  tglopa.classList.toggle("view1");
+  screen.classList.toggle("screen1");
 }
